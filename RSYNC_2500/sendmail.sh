684a4a349a5d7c3826eabb41225f41caa27041ff
小小神 <linjilee@sina.com>
2018-01-17 22:32:50 +0800

    Update sendmail.sh

--- RSYNC_2500/sendmail.sh
+++ RSYNC_2500/sendmail.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-/usr/bin/sendEmail  -f P_tj_hpc@genomics.cn -t lilinji@genomics.cn -t liuhaisheng@genomics.cn -cc yangmeng@genomics.cn  -s mail.genomics.cn  -u "Hiseq2500-D177-Report" -xu P_tj_hpc -xp LLJllj123 -o message-file=/root/RSYNC_2500/body.txt  -a /root/log/rsync_log.txt -a /root/log/rsync_log2.txt
+/usr/bin/sendEmail  -f P_tj_hpc@genomics.cn -t lilinji@genomics.cn -t liuhaisheng@genomics.cn -cc yangmeng@genomics.cn  -s mail.genomics.cn  -u "Hiseq2500-D177-Report" -xu P_tj_hpc -xp LLJ -o message-file=/root/RSYNC_2500/body.txt  -a /root/log/rsync_log.txt -a /root/log/rsync_log2.txt
 sleep  3
 /root/RSYNC_2500/rsync_tmbackup.sh  /root/log/  /data/back_log >/dev/null 2>&1
 sleep  3
